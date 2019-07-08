f34c7c657cf359a8995382f92b19b7aeea5fb17b
bhargav422 <52648776+bhargav422@users.noreply.github.com>
2019-07-08 12:43:33 +0530

    Update and rename README.md to starting.c

--- README.md
+++
@@ -1,4 +0,0 @@
-# hello-world
-To make a project with C
-first thing is to create a compiler
-
---
+++ starting.c
@@ -0,0 +1,9 @@
+# hello-world
+#include<stdio.h>
+main()
+{
+int a=5,b=4,c;
+c = a+b;
+print("Addition result for c= %d",c);
+return 0;
+}
